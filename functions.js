ae3b6f104c40a523294f16b18372fac43d933d21
StoneHeart99 <49852512+StoneHeart99@users.noreply.github.com>
2020-04-29 17:47:30 +0800

    NN lesson

--- functions.js
+++ functions.js
@@ -22,6 +22,18 @@ function clearArray(array) {
     }
 }
 
+function printArray(array) {
+    let string = "";
+    for(let i=0; i<array.length;i++)
+    {
+        string += array[i];
+
+        if(i!=array.length-1)
+            string += ", ";
+    }
+    return string;
+}
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
